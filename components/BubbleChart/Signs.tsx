e79efa25b32ea425c27dbb93607e91c6c1d3f754
Shay Culpepper <shaypepper@gmail.com>
2022-12-20 21:27:18 -0500

    Revert "better opening message2"
    This reverts commit 6f99fe6a105f403c874de753977c57b4643532ce.

--- components/BubbleChart/Signs.tsx
+++ components/BubbleChart/Signs.tsx
@@ -93,7 +93,7 @@ const Signs: FC<{
           //   setCurrentStep(SignSteps.UPLOAD_WORKERS)
           // }}
         >
-          Click on a gropuing or issue to zoom in/out
+          Click on a gropuing or issue for more detail
         </SignMenuItem>
         {/* <SignMenuItem
           index={1}
